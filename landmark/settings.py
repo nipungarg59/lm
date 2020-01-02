4e7566b8d0d5c3b58c81de01f97257b3fa915ab5
Nipun Garg <nipun.garg@flipkart.com>
2020-01-03 02:35:19 +0530

    minor

--- landmark/settings.py
+++ landmark/settings.py
@@ -70,6 +70,13 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'landmark.wsgi.application'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'djreact/static'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
